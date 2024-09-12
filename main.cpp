952cbafdc96630b02c367169c5dea35ea6ae1e6d
adankevych <adankevych@kse.org.ua>
2024-09-12 22:16:29 +0300

    file parser

--- main.cpp
+++ main.cpp
@@ -1,6 +1,74 @@
 #include <iostream>
 #include <string>
+#include <map>
+#include <fstream>
+
 using namespace std;
+
+class Parser{
+public:
+    string date;
+    string flightNumber;
+    map<int, map<int,int>> rowColumnPrice;
+
+    static void parseRecord(){
+        Parser record;
+        const char *path = "/Users/anastasia_d/CLionProjects/Airflight-Booking-System/records";
+        fstream records(path);
+        if (!records.is_open()) {
+            cout << "Could not open file." << endl;
+            return;
+        }
+
+        string line;
+
+        while (getline(records, line)) {
+            size_t position;
+
+            position = line.find(' ');
+            record.date = line.substr(0, position);
+            string lineRemainder = line.substr(position + 1);
+
+            position = lineRemainder.find(' ');
+            record.flightNumber = lineRemainder.substr(0, position);
+            lineRemainder = lineRemainder.substr(position + 1);
+
+            position = lineRemainder.find(' ');
+            int row = stoi(lineRemainder.substr(0, position));
+            lineRemainder = lineRemainder.substr(position + 1);
+
+            while ((position = lineRemainder.find(' ')) != string::npos) {
+                string currentLine = lineRemainder.substr(0, position);
+                size_t dash = currentLine.find('-');
+                int startColumn = stoi(currentLine.substr(0, dash));
+                int endColumn = stoi(currentLine.substr(dash + 1));
+
+                lineRemainder = lineRemainder.substr(position + 1);
+
+                position = lineRemainder.find(' ');
+                string dollar = lineRemainder.substr(0, position);
+                int price = stoi(dollar.substr(0, dollar.size() - 1));
+
+                lineRemainder = lineRemainder.substr(position + 1);
+
+                for (int i = startColumn; i <= endColumn; ++i) {
+                    record.rowColumnPrice[row][i] = price;
+                }
+            }
+            cout << "Date: " << record.date << ", Flight Number: " << record.flightNumber << endl;
+
+            for (auto& rowColumnPrice : record.rowColumnPrice) {
+                cout << "Row: " << rowColumnPrice.first << endl;
+                for (auto& columnPrice : rowColumnPrice.second) {
+                    cout << "Column: " << columnPrice.first << ", Price: " << columnPrice.second << endl;
+                }
+            }
+            cout << endl;
+        }
+
+    }
+
+};
 
 int main() {
     cout << "Welcome to air-flight booking system!" << endl
@@ -34,6 +102,8 @@ int main() {
     } else {
         cout << "Invalid option!" << endl;
     }
+
+    Parser::parseRecord();
 
     return 0;
 }
